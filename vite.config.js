db80e7c19e7157dd0b4da9293e7f79beb760c922
Andres Felipe Ladino - Apre <call1@izc.com.co.local>
2025-01-17 09:19:39 -0500

    other changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TicAméricas/', // Asegúrate de incluir las barras al inicio y al final
-})
+  base: '/TicAmericas/', // Nombre exacto del repositorio sin acentos
+});
