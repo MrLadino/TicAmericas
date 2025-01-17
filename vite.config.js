153680ec245a7099c9ca130a5bb077fd91721c23
Andres Felipe Ladino - Apre <call1@izc.com.co.local>
2025-01-17 08:38:42 -0500

    Update Vite config: set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TicAméricas/', // Asegúrate de incluir las barras al inicio y al final
 })
