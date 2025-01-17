e1560017f566e86c5eaa857adf2de71ca87928e1
Andres Felipe Ladino - Apre <call1@izc.com.co.local>
2025-01-17 13:19:47 -0500

    Actualizar vite a la versión más reciente

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/TicAmericas/', // Nombre exacto del repositorio sin acentos
+// });
+
 export default defineConfig({
   plugins: [react()],
-  base: '/TicAmericas/', // Nombre exacto del repositorio sin acentos
+  base: '/TicAmericas/',  // Nombre del repositorio
+  build: {
+    outDir: 'build',  // Asegúrate de que se genere en la carpeta 'build'
+  },
 });
